d13f5302bb60ee585b83b98703392ec90098bc7f
Juan David Garzón <91800633+garzonjuaan@users.noreply.github.com>
2023-12-04 15:57:56 -0500

    primary

--- src/app/maps/pages/markers-page/markers-page.component.ts
+++ src/app/maps/pages/markers-page/markers-page.component.ts
@@ -1,10 +1,14 @@
-import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
-import {LngLat, Map, Marker} from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
-import { reduce } from 'rxjs';
+import { Component, ElementRef, ViewChild, OnDestroy } from '@angular/core';
+import { Map, LngLat, Marker } from 'mapbox-gl';
 
-interface MarkerAndColor{
+interface MarkerAndColor {
   color: string;
   marker: Marker;
+}
+
+interface PlainMarker {
+  color: string;
+  lngLat: number[]
 }
 
 
@@ -12,62 +16,109 @@ interface MarkerAndColor{
   templateUrl: './markers-page.component.html',
   styleUrls: ['./markers-page.component.css']
 })
-export class MarkersPageComponent implements AfterViewInit {
+export class MarkersPageComponent {
+
+  @ViewChild('map') divMap?: ElementRef;
 
   public markers: MarkerAndColor[] = [];
 
 
   public map?: Map;
-  public lngLat: LngLat = new LngLat(-74.08175, 4.60971)
-
-  @ViewChild('map') divMap?: ElementRef
+  public currentLngLat: LngLat = new LngLat(-74.10380784179445, 4.651165392795477);
 
 
   ngAfterViewInit(): void {
 
-    if(!this.divMap) throw 'Elemento HTML no fue encontrado'
+    if ( !this.divMap ) throw 'El elemento HTML no fue encontrado';
 
     this.map = new Map({
-      container: 'map', // container ID
+      container: this.divMap.nativeElement, // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
-      center: this.lngLat, // starting position [lng, lat]
-      zoom: 13, // starting zoom
+      center: this.currentLngLat,
+      zoom: 13
     });
+    this.readFromLocalStorage();
+
+    // const markerHtml = document.createElement('div');
+    // markerHtml.innerHTML = 'Fernando Herrera'
+
+    // const marker = new Marker({
+    //   // color: 'red',
+    //   element: markerHtml
+    // })
+    //   .setLngLat( this.currentLngLat )
+    //   .addTo( this.map );
+
   }
 
-  createMarker(){
-
-    if(!this.map) return;
+  createMarker() {
+    if ( !this.map ) return;
 
     const color = '#xxxxxx'.replace(/x/g, y=>(Math.random()*16|0).toString(16));
-    const lgnLat = this.map.getCenter()
+    const lngLat = this.map.getCenter();
 
-    this.addMarker( lgnLat, color );
+    this.addMarker( lngLat, color );
   }
 
-  addMarker( lgnLat: LngLat, color: string ){
-    if( !this.map ) return;
+
+  addMarker( lngLat: LngLat, color: string ) {
+    if ( !this.map ) return;
 
     const marker = new Marker({
       color: color,
       draggable: true
-    }).setLngLat(lgnLat)
-      .addTo( this.map )
+    })
+      .setLngLat( lngLat )
+      .addTo( this.map );
 
-      this.markers.push({ color, marker  })
+    this.markers.push({ color, marker, });
+    this.saveToLocalStorage();
 
+    marker.on('dragend', () => this.saveToLocalStorage() );
+
+    // dragend
   }
 
-  deleteMarker(index: number){
-    this.markers[index].marker.remove()
-    this.markers.splice(index, 1)
-  }
+  deleteMarker( index: number ) {
+    this.markers[index].marker.remove();
+    this.markers.splice(index, 1);
+    this.saveToLocalStorage()
+    }
 
-  flyTo(marker: Marker){
+  flyTo( marker: Marker ) {
+
     this.map?.flyTo({
       zoom: 14,
       center: marker.getLngLat()
-    })
+    });
+
   }
+
+
+  saveToLocalStorage() {
+    const plainMarkers: PlainMarker[] = this.markers.map( ({ color, marker }) => {
+      return {
+        color,
+        lngLat: marker.getLngLat().toArray()
+      }
+    });
+
+    localStorage.setItem('plainMarkers', JSON.stringify( plainMarkers ));
+
+  }
+
+  readFromLocalStorage() {
+    const plainMarkersString = localStorage.getItem('plainMarkers') ?? '[]';
+    const plainMarkers: PlainMarker[] = JSON.parse( plainMarkersString ); //! OJO!
+
+    plainMarkers.forEach( ({ color, lngLat }) => {
+      const [ lng, lat ] = lngLat;
+      const coords = new LngLat( lng, lat );
+
+      this.addMarker( coords, color );
+    })
+
+  }
+
 
 }
